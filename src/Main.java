77b2ae681fe742f7be6ee663116fbffb37f36ab5
Paulo Lopes <paulo.hq.lopes@gmail.com>
2018-05-11 11:39:53 +0000

    KickOff

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Bathroom bath = new Bathroom.Builder()
-				.setBathCapacity(10)
+				.setBathCapacity(BATH_CAPACITY)
 				.build();
 
 		System.out.println(bath.getBathList().get(0).whosThere());
