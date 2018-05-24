1055a7af80b1906fa05c28f05883f5899517162e
Paulo Lopes <paulo.hq.lopes@gmail.com>
2018-05-23 23:55:01 -0300

    Update Bathroom.java

--- src/wc/Bathroom.java
+++ src/wc/Bathroom.java
@@ -2,119 +2,134 @@ package wc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Semaphore;
+import java.util.stream.Collectors;
 
 import wc.utils.Gender;
 import wc.utils.Person;
-import wc.utils.Toilet;
 
 /**
  * Office Bathroon Class, detain all biombos.
- * 
- * @author paulo
  *
+ * @author paulo
  */
 public class Bathroom {
-	
-	private final int bathCapacity;
-	private List<Person> usersList;
-	private List<Person> waitingList;
+    private Semaphore semaphore = new Semaphore(1);
 
-	private static Bathroom bath;
+    private final int bathCapacity;
+    private List<Person> usersList;
+    private List<Person> waitingList;
 
-	private Bathroom(int capacity, int waitingListCapacity) {
-		this.bathCapacity = capacity;
-		this.waitingList = new ArrayList<>(waitingListCapacity);
-		this.usersList = new ArrayList<>(capacity);
-	}
+    private static Bathroom bath;
 
-	public long getOccupancy() {
-		long aux = this.getUsersList().stream()
-			.filter(a -> a.getGender() != Gender.EMPTY).count();
-			
-		
-		System.out.println("Currently the total occupancy is: " + aux + "/" +this.getUsersList().size());
-		return aux;
-	}
-	
-	
-	public void addUser(Person p){
-		usersList.add(p);
-	}
-	
-	public Boolean isPersonThere(Person p){
-		return usersList.contains(p);
-	}
-	
+    private Bathroom(int capacity, int waitingListCapacity) {
+        this.bathCapacity = capacity;
+        this.waitingList = new ArrayList<>(waitingListCapacity);
+        this.usersList = new ArrayList<>(capacity);
+    }
 
-	public void removePerson(Person person) {
-		usersList.remove(person);
-	}
+    public long getOccupancy() {
+        System.out.println("[" + Thread.currentThread().getName() + "] Currently the occupancy is: " + usersList.size() + "/" + bathCapacity);
+        return usersList.size();
+    }
+
+    public Semaphore getSemaphore() {
+        return semaphore;
+    }
+
+    /**
+     * Here we must insert smart blocks to see if one person could enter
+     *
+     * @param p
+     */
+    public void addUser(Person p) {
+        if(bathCapacity > usersList.size()){
+            usersList.add(p);
+            System.out.println(p.getName() + " enters in the bath.");
+        }else {
+            System.out.println(p.getName() + " The bath is totally occuped.");
+        }
 
 
-	public List<Person> getUsersList() {
-		return usersList;
-	}
+    }
 
-	public List<Person> getWaitingList() {
-		return waitingList;
-	}
 
-	public static Bathroom getBath() {
-		return bath;
-	}
+    public Boolean isPersonThere(Person p) {
 
-	public int getBathCapacity() {
-		return bathCapacity;
-	}
-	
-	public static Bathroom getInstance(){
-		if (bath == null){
-			bath = Builder.build();
-		}
-		return bath;
-	}
-	
-	
+        return usersList.contains(p);
 
-	/**
-	 * Bathroom Builder if the bathroom increases in attributes, just put these
-	 * here.
-	 * 
-	 * @author paulo
-	 *
-	 */
-	public static class Builder {
+    }
 
-		private static int bathCapacity;
-		private static int queueCapacity;
 
-		public Builder setBathCapacity(int capacity) {
-			this.bathCapacity = capacity;
-			return this;
-		}
+    public void removePerson(Person person) {
+        usersList.remove(person);
+        System.out.println(person.getName() + " goes out");
 
-		public Builder setQueueCapacity(int qCapacity) {
-			this.queueCapacity = qCapacity;
-			return this;
-		}
+    }
 
-		/**
-		 * starts a Bathroom with empty toilets
-		 * 
-		 * @return
-		 */
-		public static Bathroom build() {
-			if (bath == null) {
-				synchronized (Bathroom.class) {
-					if (bath == null) {
-						bath = new Bathroom(bathCapacity, queueCapacity);
-					}
-				}
-			}
-			return bath;
-		}
-	}
 
-	
+    public List<Person> getUsersList() {
+        return usersList;
+    }
+
+    public List<Person> getWaitingList() {
+        return waitingList;
+    }
+
+    public static Bathroom getBath() {
+        return bath;
+    }
+
+    public int getBathCapacity() {
+        return bathCapacity;
+    }
+
+    public static Bathroom getInstance() {
+        if (bath == null) {
+            bath = Builder.build();
+        }
+        return bath;
+    }
+
+
+    /**
+     * Bathroom Builder if the bathroom increases in attributes, just put these
+     * here.
+     *
+     * @author paulo
+     */
+    public static class Builder {
+
+        private static int bathCapacity;
+        private static int queueCapacity;
+
+        public Builder setBathCapacity(int capacity) {
+            this.bathCapacity = capacity;
+            return this;
+        }
+
+        public Builder setQueueCapacity(int qCapacity) {
+            this.queueCapacity = qCapacity;
+            return this;
+        }
+
+        /**
+         * starts a Bathroom with empty toilets
+         *
+         * @return
+         */
+        public static Bathroom build() {
+            if (bath == null) {
+                synchronized (Bathroom.class) {
+                    if (bath == null) {
+                        bath = new Bathroom(bathCapacity, queueCapacity);
+                    }
+                }
+            }
+            return bath;
+        }
+    }
+
 
 }
